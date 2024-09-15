e7f8ddcbbd60881b2f2b943c2f931a4d562d5d6a
Kaspener <daniil.shpilev@gmail.com>
2024-09-15 18:51:59 +0700

    Renderer

--- app/src/main/java/com/example/astronomicalreferencebook/MainActivity.kt
+++ app/src/main/java/com/example/astronomicalreferencebook/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.astronomicalreferencebook
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AdvertisementWindow()
-        }
+
+        val glSurfaceView = MyGLSurfaceView(this)
+        setContentView(glSurfaceView)
     }
 }
